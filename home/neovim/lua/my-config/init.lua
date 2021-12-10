adbedf1791b26c462a2982ed6daad4b95cc3fc0f
Lite Ye <yelite958@gmail.com>
2021-12-10 00:33:54 -0500

    Tweak auto save

--- home/neovim/lua/my-config/init.lua
+++ home/neovim/lua/my-config/init.lua
@@ -79,7 +79,7 @@ require("session-lens").setup {}
 require("autosave").setup {
     enabled = true,
     execution_message = "",
-    events = { "BufLeave", "CursorHold" },
+    events = { "BufLeave", "FocusLost", "CursorHold" },
     conditions = {
         exists = true,
         filename_is_not = {},
@@ -89,7 +89,7 @@ require("autosave").setup {
     write_all_buffers = false,
     on_off_commands = false,
     clean_command_line_interval = 0,
-    debounce_delay = 500,
+    debounce_delay = 150,
 }
 
 require("gitsigns").setup {
