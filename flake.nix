aecda7975c450a9d3f23c71500549189cedbd15a
Lite Ye <yelite958@gmail.com>
2021-12-18 21:55:42 -0500

    Add logiops for mouse

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           ./hosts/moonshot
           ./modules/desktop.nix
           ./modules/keyboard-remap
+          ./modules/logitech
           {
             home-manager.users.liteye.imports = [
               ./home/gui.nix
---
+++ modules/logitech/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+let
+  logiops = pkgs.callPackage ./package.nix { };
+in
+{
+  environment.systemPackages = [
+    pkgs.solaar # tool for the unify receiver
+  ];
+
+  users.users.logiops = {
+    isSystemUser = true;
+    group = "logiops";
+    extraGroups = [ "hidraw" ];
+  };
+  users.groups.logiops = { };
+  users.groups.hidraw = { };
+
+  # Taken from https://github.com/NixOS/nixpkgs/pull/124158#discussion_r716201439
+  services.udev.extraRules = '' 
+    KERNEL=="uinput", GROUP="hidraw", MODE:="0660", OPTIONS+="static_node=uinput" 
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="046d", MODE="0660", GROUP="hidraw" 
+  '';
+
+  systemd.services.logiops = {
+    description = "Logitech Configuration Daemon";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${logiops}/bin/logid -c ${./logiops.cfg}";
+      User = "logiops";
+      ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+}
