5f720857d69d8fa388ea0486f73975308ddc86d8
Lite Ye <yelite958@gmail.com>
2021-12-27 23:29:25 -0500

    Update neoclip config to latest format

--- home/modules/neovim/lua/my-config/init.lua
+++ home/modules/neovim/lua/my-config/init.lua
@@ -153,17 +153,19 @@ require("neoclip").setup {
         set_reg = false,
     },
     keys = {
-        i = {
-            select = "<cr>",
-            paste = "<C-CR>",
-            paste_behind = "<c-k>",
-            custom = {},
-        },
-        n = {
-            select = "<cr>",
-            paste = "p",
-            paste_behind = "P",
-            custom = {},
+        telescope = {
+            i = {
+                select = "<cr>",
+                paste = "<C-CR>",
+                paste_behind = "<c-k>",
+                custom = {},
+            },
+            n = {
+                select = "<cr>",
+                paste = "p",
+                paste_behind = "P",
+                custom = {},
+            },
         },
     },
 }
