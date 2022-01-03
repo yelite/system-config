2143b1662b1614ec5931e602e1f908c2c53a8069
Lite Ye <yelite958@gmail.com>
2022-01-02 23:37:09 -0500

    Refactor extract nvim plugin function to make it cleaner

--- modules/programs/neovim/extract-extra-plugins.nix
+++ modules/programs/neovim/extract-extra-plugins.nix
@@ -1,22 +1,27 @@
 let
-  listToAttrs = builtins.listToAttrs;
-  attrToList = (set: map (name: { name = name; value = set.${name}; }) (builtins.attrNames set));
-  filter = builtins.filter;
-  stringLength = builtins.stringLength;
-  substring = builtins.substring;
-
+  inherit (builtins) listToAttrs attrNames filter stringLength substring;
+  attrToList = set: map (name: { name = name; value = set.${name}; }) (attrNames set);
   prefix = "neovim";
   prefixLength = stringLength prefix;
-
-  # Here it extract all inputs with prefix in their names, and build them as vim plugins
-  extractPluginPackages = (pkgs: inputs: listToAttrs (map
-    (item:
-      let name = substring (prefixLength + 1) (stringLength item.name) item.name; in
-      {
-        name = name;
-        value = pkgs.vimUtils.buildVimPluginFrom2Nix { pname = name; version = "flake"; src = item.value; };
-      })
-    (filter (item: (substring 0 prefixLength item.name) == prefix)
-      (attrToList inputs))));
+  isPluginInput = item: (substring 0 prefixLength item.name) == prefix;
+  getPluginName = inputName: substring (prefixLength + 1) (stringLength inputName) inputName;
 in
-extractPluginPackages
+# Here it extract all inputs with prefix in their names, and build them as vim plugins
+(pkgs: inputs:
+  let
+    pluginInputs = filter isPluginInput (attrToList inputs);
+  in
+  listToAttrs (map
+    (item:
+      let name = getPluginName item.name; in
+      {
+        inherit name;
+        value = pkgs.vimUtils.buildVimPluginFrom2Nix {
+          pname = name;
+          version = "flake";
+          src = item.value;
+        };
+      }
+    )
+    pluginInputs
+  ))
