891d930dab5f74ed8875d2af2dd02505d689cd05
Lite Ye <yelite958@gmail.com>
2022-02-03 21:06:58 -0500

    Upgrade packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,4 +4,11 @@ final: prev: {
   apple-cursor = prev.callPackage ./apple-cursor.nix { };
   neovide = prev.callPackage ./neovide.nix { inherit (prev) neovide; };
   xremap = prev.callPackage ./xremap.nix { };
+  alsa-lib = prev.alsa-lib.overrideAttrs (prev: {
+    patches = [
+      # This is a workaround for the alsa bug before
+      # https://github.com/NixOS/nixpkgs/pull/157631 landing in unstable
+      ./alsa.patch
+    ];
+  });
 }
