e4dd2772bad7d504ba36cfc4b185088868ff6bb9
Lite Ye <yelite958@gmail.com>
2022-04-18 15:09:14 -0400

    Fix tide config

--- modules/programs/fish/config.fish
+++ modules/programs/fish/config.fish
@@ -16,6 +16,6 @@ set -g tide_left_prompt_frame_enabled true
 set -g tide_left_prompt_items pwd git newline character
 set -g tide_right_prompt_frame_enabled false
 set -g tide_right_prompt_items status cmd_duration context jobs node virtual_env rustc java php chruby go kubectl toolbox terraform nix_shell
-set -U tide_prompt_color_frame_and_connection 555 brblack
+set -g tide_prompt_color_frame_and_connection 555 brblack
 
 set -g sponge_delay 5
