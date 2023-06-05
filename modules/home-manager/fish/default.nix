45efffa1315c6cde629e4f680f66e060f001e38d
Lite Ye <yelite958@gmail.com>
2023-06-05 15:17:48 -0400

    [Fish] Starship config

--- modules/home-manager/fish/default.nix
+++ modules/home-manager/fish/default.nix
@@ -28,6 +28,26 @@ in
     programs.starship = {
       enable = true;
       enableFishIntegration = true;
+      settings = {
+        format = lib.concatStrings [
+          "$username"
+          "$hostname"
+          "$directory"
+          "$git_branch"
+          "$git_commit"
+          "$git_state"
+          "$git_metrics"
+          "$git_status"
+          "$hg_branch"
+          "$package"
+          "$nix_shell"
+          "$sudo"
+          "$cmd_duration"
+          "$line_break"
+          "$jobs"
+          "$character"
+        ];
+      };
     };
 
     xdg.configFile."bat/config".source = pkgs.writeText "bat_config"
