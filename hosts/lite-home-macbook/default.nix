538ac7554ba964dea4e18b9fd73007ae9d66b009
Lite Ye <yelite958@gmail.com>
2024-02-05 19:18:25 -0500

    [Darwin] Add linux builder vm

--- hosts/lite-home-macbook/default.nix
+++ hosts/lite-home-macbook/default.nix
@@ -1,6 +1,10 @@
 {config, ...}: let
   myConfig = config.myConfig;
 in {
+  # TODO: move builder to home server
+  nix.linux-builder.enable = true;
+  nix.settings.trusted-users = [myConfig.username];
+
   myConfig.homeManagerConfig = {
     neovim.enable = true;
     kitty.enable = true;
