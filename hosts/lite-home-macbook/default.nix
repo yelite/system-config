e3da418461fecc82bbf27717175b42a6793df40a
Lite Ye <yelite958@gmail.com>
2024-02-06 23:48:42 -0500

    [Host] Remove linux builder in mac

--- hosts/lite-home-macbook/default.nix
+++ hosts/lite-home-macbook/default.nix
@@ -1,10 +1,4 @@
-{config, ...}: let
-  myConfig = config.myConfig;
-in {
-  # TODO: move builder to home server
-  nix.linux-builder.enable = true;
-  nix.settings.trusted-users = [myConfig.username];
-
+{
   myConfig.homeManagerConfig = {
     neovim.enable = true;
     kitty.enable = true;
