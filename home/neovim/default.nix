d2528741b527953d771d1a246a4915d065441556
Lite Ye <yelite958@gmail.com>
2024-07-02 00:22:18 -0400

    cleanup neovim nix file

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -5,8 +5,7 @@
   hostPlatform,
   ...
 }:
-with lib; let
-  cfg = config.myConfig.neovim;
+lib.mkIf config.myConfig.neovim.enable (let
   sqlite3_lib_path =
     if pkgs.stdenv.isLinux
     then "${pkgs.sqlite.out}/lib/libsqlite3.so"
@@ -53,163 +52,162 @@ with lib; let
       yuck
       vim
     ]);
-in
-  mkIf cfg.enable {
-    home.packages = lib.optionals hostPlatform.isLinux [
-      pkgs.neovide
+in {
+  home.packages = lib.optionals hostPlatform.isLinux [
+    pkgs.neovide
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.neovim = {
+    enable = true;
+
+    plugins = with pkgs.vimPlugins; [
+      plenary-nvim
+      popup-nvim
+
+      my-treesitter
+      nvim-treesitter-textobjects
+      playground # treesitter-playground
+
+      gbprod-nord
+      nvim-web-devicons
+      pets-nvim
+      hologram-nvim # dependency of pets-nvim
+      nui-nvim # dependency of pets-nvim
+      dressing-nvim
+      hlchunk-nvim
+      gitsigns-nvim
+      bufdelete-nvim
+      gitlinker-nvim
+
+      leap-nvim
+      flash-nvim
+      hop-nvim
+      hop-extensions
+      nvim-autopairs
+      vim-repeat
+      vim-surround
+      vim-subversive
+      vim-textobj-entire
+      nvim-spectre
+      bullets-vim
+
+      lualine-nvim
+      nvim-navic
+      telescope-nvim
+      telescope-fzf-native-nvim
+      telescope-file-browser-nvim
+      telescope-live-grep-args-nvim
+      telescope-alternate
+      toggleterm-nvim
+      which-key-nvim
+      smart-open-nvim
+
+      autosave
+      possession-nvim
+      nvim-neoclip-lua
+
+      zen-mode-nvim
+      glow-nvim
+      obsidian-nvim
+
+      nvim-lspconfig
+      fidget-nvim
+      lspsaga-nvim
+      trouble-nvim
+      aerial-nvim
+      nvim-dap
+      nvim-cmp
+      cmp-buffer
+      cmp-nvim-lsp
+      cmp-path
+      cmp-cmdline
+      cmp_luasnip
+      luasnip
+      copilot-lua
+      copilot-cmp
+      lspkind-nvim
+      lsp_signature-nvim
+
+      todo-comments-nvim
+      comment-nvim
+      null-ls-nvim
+      lazydev-nvim
+      rust-tools-nvim
+      clangd_extensions-nvim
+      go-nvim
+      SchemaStore-nvim
+
+      {
+        plugin = sqlite-lua;
+        config = "let g:sqlite_clib_path = '${sqlite3_lib_path}'";
+      }
     ];
 
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
+    extraConfig = ''
+      lua << END🤞
+        vim.g._my_config_script_folder = "${./scripts}"
+        vim.g._friendly_snippets_path = "${pkgs.vimPlugins.friendly-snippets}"
+        require "my-config"
+      END🤞
+    '';
 
-    programs.neovim = {
-      enable = true;
+    extraPackages = with pkgs;
+      [
+        # for treesitter
+        gcc
+        fd
+        ripgrep
+        stylua
+        graphviz # For rust crate graph visualization
+        sqlite # For sqlite.lua
 
-      plugins = with pkgs.vimPlugins; [
-        plenary-nvim
-        popup-nvim
+        glow
 
-        my-treesitter
-        nvim-treesitter-textobjects
-        playground # treesitter-playground
+        clang-tools
+        rust-analyzer
+        rustfmt
 
-        gbprod-nord
-        nvim-web-devicons
-        pets-nvim
-        hologram-nvim # dependency of pets-nvim
-        nui-nvim # dependency of pets-nvim
-        dressing-nvim
-        hlchunk-nvim
-        gitsigns-nvim
-        bufdelete-nvim
-        gitlinker-nvim
+        gopls
+        golines
+        gofumpt
+        golangci-lint
+        impl
+        reftools
+        delve
+        gomodifytags
 
-        leap-nvim
-        flash-nvim
-        hop-nvim
-        hop-extensions
-        nvim-autopairs
-        vim-repeat
-        vim-surround
-        vim-subversive
-        vim-textobj-entire
-        nvim-spectre
-        bullets-vim
+        nil
+        alejandra
+        taplo
+        sumneko-lua-language-server
+        cmake-language-server
+        buf
+        buf-language-server
+        python3Packages.jedi-language-server
+        marksman
 
-        lualine-nvim
-        nvim-navic
-        telescope-nvim
-        telescope-fzf-native-nvim
-        telescope-file-browser-nvim
-        telescope-live-grep-args-nvim
-        telescope-alternate
-        toggleterm-nvim
-        which-key-nvim
-        smart-open-nvim
-
-        autosave
-        possession-nvim
-        nvim-neoclip-lua
-
-        zen-mode-nvim
-        glow-nvim
-        obsidian-nvim
-
-        nvim-lspconfig
-        fidget-nvim
-        lspsaga-nvim
-        trouble-nvim
-        aerial-nvim
-        nvim-dap
-        nvim-cmp
-        cmp-buffer
-        cmp-nvim-lsp
-        cmp-path
-        cmp-cmdline
-        cmp_luasnip
-        luasnip
-        copilot-lua
-        copilot-cmp
-        lspkind-nvim
-        lsp_signature-nvim
-
-        todo-comments-nvim
-        comment-nvim
-        null-ls-nvim
-        lazydev-nvim
-        rust-tools-nvim
-        clangd_extensions-nvim
-        go-nvim
-        SchemaStore-nvim
-
-        {
-          plugin = sqlite-lua;
-          config = "let g:sqlite_clib_path = '${sqlite3_lib_path}'";
-        }
+        nodePackages.nodejs
+        nodePackages.prettier
+        nodePackages.yaml-language-server
+        nodePackages.vscode-langservers-extracted
+        vtsls
+        nodePackages."@astrojs/language-server"
+        nodePackages."@tailwindcss/language-server"
+      ]
+      ++ lib.optionals hostPlatform.isDarwin [
+        pngpaste # For ObsidianPasteImg
       ];
 
-      extraConfig = ''
-        lua << END🤞
-          vim.g._my_config_script_folder = "${./scripts}"
-          vim.g._friendly_snippets_path = "${pkgs.vimPlugins.friendly-snippets}"
-          require "my-config"
-        END🤞
-      '';
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withPython3 = true;
+    withRuby = false;
+  };
 
-      extraPackages = with pkgs;
-        [
-          # for treesitter
-          gcc
-          fd
-          ripgrep
-          stylua
-          graphviz # For rust crate graph visualization
-          sqlite # For sqlite.lua
-
-          glow
-
-          clang-tools
-          rust-analyzer
-          rustfmt
-
-          gopls
-          golines
-          gofumpt
-          golangci-lint
-          impl
-          reftools
-          delve
-          gomodifytags
-
-          nil
-          alejandra
-          taplo
-          sumneko-lua-language-server
-          cmake-language-server
-          buf
-          buf-language-server
-          python3Packages.jedi-language-server
-          marksman
-
-          nodePackages.nodejs
-          nodePackages.prettier
-          nodePackages.yaml-language-server
-          nodePackages.vscode-langservers-extracted
-          vtsls
-          nodePackages."@astrojs/language-server"
-          nodePackages."@tailwindcss/language-server"
-        ]
-        ++ lib.optionals hostPlatform.isDarwin [
-          pngpaste # For ObsidianPasteImg
-        ];
-
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      withPython3 = true;
-      withRuby = false;
-    };
-
-    xdg.configFile."nvim/lua".source = ./lua;
-  }
+  xdg.configFile."nvim/lua".source = ./lua;
+})
