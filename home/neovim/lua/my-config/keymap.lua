c90c898ce7969882a87ca1f13fd71063e06d7a00
Lite Ye <yelite958@gmail.com>
2024-09-10 20:25:56 -0400

    neovim: use the new whichkey api

--- home/neovim/lua/my-config/keymap.lua
+++ home/neovim/lua/my-config/keymap.lua
@@ -14,6 +14,7 @@ vim.g.mapleader = " "
 vim.o.timeoutlen = 1200
 
 wk.setup({
+    preset = "helix",
     win = { border = "single" },
     icons = { separator = " " },
     layout = {
@@ -21,6 +22,15 @@ wk.setup({
         width = { max = 36 },
     },
 })
+
+local function goto_start_of_line()
+    local current_col = vim.api.nvim_win_get_cursor(0)[2]
+    vim.api.nvim_command([[normal! ^]])
+    local new_col = vim.api.nvim_win_get_cursor(0)[2]
+    if new_col == current_col then
+        vim.api.nvim_command([[normal! 0]])
+    end
+end
 
 local function copy_expansion_result(s)
     local path = vim.fn.expand(s)
@@ -72,220 +82,162 @@ wk.add({
     { "<leader>x", "<cmd>Telescope commands<cr>", desc = "Commands" },
     { "<leader>.", "<cmd>Telescope resume<cr>", desc = "Resume Last Telescope Picker" },
     { "<leader>>", "<cmd>Telescope pickers<cr>", desc = "Previous Telescope Pickers" },
-})
 
--- e -> edit
-local edit_keymap = {
-    name = "edit",
-    s = { "<plug>(SubversiveSubstituteWordRange)", "Substitute Word in Range" },
-    S = { "<plug>(SubversiveSubstituteRange)", "Substitue Range" },
-    t = { "<cmd>Telescope spell_suggest<cr>", "Spell Suggests" },
-    p = { "<cmd>Telescope neoclip<cr>", "Clipboard History" },
-    m = { "<cmd>Glow<cr>", "Preview Markdown" },
-}
--- f -> file
-local file_keymap = {
-    name = "file",
-    e = {
-        "<cmd>lua require'telescope'.extensions.file_browser.file_browser{path='%:p:h'}<cr>",
-        "Browser in current directory",
+    { "<leader>b", group = "buffer" },
+    { "<leader>bb", [[<cmd>Telescope buffers<cr>]], desc = "Switch Buffer" },
+    { "<leader>bd", [[<cmd>Bdelete<cr>]], desc = "Close Buffer" },
+    { "<leader>bD", [[<cmd>Bdelete!<cr>]], desc = "Force Close Buffer" },
+    { "<leader>bn", [[<cmd>bn<cr>]], desc = "Next Buffer" },
+    { "<leader>bp", [[<cmd>bp<cr>]], desc = "Previous Buffer" },
+
+    { "<leader>e", group = "edit" },
+    { "<leader>em", [[<cmd>Glow<cr>]], desc = "Preview Markdown" },
+    { "<leader>ep", [[<cmd>Telescope neoclip<cr>]], desc = "Clipboard History" },
+    { "<leader>es", [[<plug>(SubversiveSubstituteWordRange)]], desc = "Substitute Word in Range" },
+    { "<leader>eS", [[<plug>(SubversiveSubstituteRange)]], desc = "Substitue Range" },
+    { "<leader>et", [[<cmd>Telescope spell_suggest<cr>]], desc = "Spell Suggests" },
+
+    { "<leader>f", group = "file" },
+    {
+        "<leader>fe",
+        [[<cmd>lua require'telescope'.extensions.file_browser.file_browser{path='%:p:h'}<cr>]],
+        desc = "Browser in current directory",
     },
-    E = { "<cmd>lua require'telescope'.extensions.file_browser.file_browser{files=false}<cr>", "Browser folders" },
-    f = {
+    {
+        "<leader>fE",
+        [[<cmd>lua require'telescope'.extensions.file_browser.file_browser{files=false}<cr>]],
+        desc = "Browser folders",
+    },
+    {
+        "<leader>ff",
         [[<cmd>lua require('telescope.builtin').find_files({cwd="%:p:h", results_title=vim.fn.expand("%:h")})<cr>]],
-        "Find Files",
+        desc = "Find Files",
     },
-    F = { "<cmd>Telescope find_files no_ignore=true<cr>", "Find All Files" },
-    r = { [[<cmd>lua require('telescope.builtin').oldfiles({only_cwd=true})<cr>]], "Recent Files" },
-    R = { [[<cmd>lua require('telescope.builtin').oldfiles({})<cr>]], "Global Recent Files" },
-    s = { "<cmd>w<cr>", "Save File" },
-    p = {
+    { "<leader>fF", [[<cmd>Telescope find_files no_ignore=true<cr>]], desc = "Find All Files" },
+    {
+        "<leader>fp",
         function()
             copy_expansion_result("%:.")
         end,
-        "Copy relative path",
+        desc = "Copy relative path",
     },
-    P = {
+    {
+        "<leader>fP",
         function()
             copy_expansion_result("%:.:h")
         end,
-        "Copy relative path of the current directory",
+        desc = "Copy relative path of the current directory",
     },
-    y = {
+    { "<leader>fr", [[<cmd>lua require('telescope.builtin').oldfiles({only_cwd=true})<cr>]], desc = "Recent Files" },
+    { "<leader>fR", [[<cmd>lua require('telescope.builtin').oldfiles({})<cr>]], desc = "Global Recent Files" },
+    { "<leader>fs", [[<cmd>w<cr>]], desc = "Save File" },
+    {
+        "<leader>fy",
         function()
             require("yazi").yazi(nil, vim.fn.expand("%:p:h"))
         end,
-        "Open yazi in cwd",
+        desc = "Open yazi in cwd",
     },
-    Y = {
+    {
+        "<leader>fY",
         function()
             require("yazi").yazi(nil, vim.fn.getcwd())
         end,
-        "Open yazi in cwd",
+        desc = "Open yazi in cwd",
     },
-}
--- b -> buffer
-local buffer_keymap = {
-    name = "buffer",
-    b = { "<cmd>Telescope buffers<cr>", "Switch Buffer" },
-    d = { "<cmd>Bdelete<cr>", "Close Buffer" },
-    D = { "<cmd>Bdelete!<cr>", "Force Close Buffer" },
-    n = { "<cmd>bn<cr>", "Next Buffer" },
-    p = { "<cmd>bp<cr>", "Previous Buffer" },
-}
--- w -> window
-local window_keymap = {
-    name = "window",
-    w = { leap_to_window, "Jump to Window" },
-    o = { my_window.move_to_next_window, "Move Buffer to Next Window" },
-    O = { my_window.open_in_next_window, "Open Buffer in Next Window" },
-    p = {
-        "<cmd>lua require'my-config.window'.move_to_next_window(true)<cr>",
-        "Move Buffer to Next Window And Enter",
-    },
-}
--- i -> code intelligence
-local code_keymap = {
-    name = "code actions",
-    -- TODO: Move to lsp on_attach
-    s = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
-    S = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-    d = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-    D = { "<cmd>Trouble document_diagnostics<cr>", "Document Diagnostics" },
-    a = { "<cmd>Telescope lsp_range_code_actions<cr>", "Code Actions" },
-    r = { "<cmd>Lspsaga rename<cr>", "Rename Symbol" },
-}
--- s -> search
-local search_keymap = {
-    name = "search",
-    r = { "<cmd>lua require('spectre').open()<CR>", "Search and replace" },
-    R = { "viw:lua require('spectre').open_file_search()<CR>", "Search and replace current symbol" },
-    f = {
-        [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>]],
-        "Search",
-    },
-    F = {
-        [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args({cwd="%:p:h", results_title=vim.fn.fnamemodify(vim.fn.expand("%:h"), ":~:.")})<cr>]],
-        "Search in the directory of current file",
-    },
-    h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymap" },
-    i = { "<cmd>Telescope treesitter<cr>", "Search Syntax Node" },
-}
--- t -> toggle / settings
-local toggle_feature_keymap = {
-    name = "toggle features",
-    b = { [[<cmd>Gitsigns toggle_current_line_blame<cr>]], "Toggle Blame Line" },
-    c = { my_util.toggle_copilot_suppression, "Toggle Copilot suppression" },
-    C = { [[<cmd>Copilot panel<cr>]], "Toggle Copilot panel" },
-    d = { [[<cmd>TroubleToggle<cr>]], "Trouble Window" },
-    n = { [[<cmd>set invnumber<cr>]], "Line Number" },
-    p = { [[<cmd>InspectTree<cr>]], "Treesitter Playground" },
-    s = { require("auto-save").toggle, "Auto Save" },
-    S = { [[<cmd>set invspell<cr>]], "Spell Check" },
-    g = {
-        name = "git",
-        b = { my_util.make_async_func(my_settings.prompt_setting, "git_base_branch"), "Set Git Base Branch" },
-        r = {
-            my_util.make_async_func(my_settings.prompt_setting, "git_alternative_remote"),
-            "Set Git Alternative Remote",
-        },
-    },
-    f = { require("zen-mode").toggle, "Focus Mode" },
-}
--- v -> version control
-local vcs_keymap = {
-    name = "version control",
-    v = { [[<cmd>lua require("my-config.terminal").toggle_lazygit()<cr>]], "Open lazygit" },
-    f = { [[<cmd>Telescope git_status<cr>]], "Git Status" },
-    s = { [[<cmd>Gitsigns stage_hunk<cr>]], "Stage Hunk" },
-    u = { [[<cmd>Gitsigns undo_stage_hunk<cr>]], "Undo Stage Hunk" },
-    l = { my_git.copy_link_to_remote, "Copy link to repo remote" },
-    L = { my_git.copy_link_to_alternative_remote, "Copy link to alternative remote" },
-    h = { [[<cmd>Gitsigns setqflist all<cr>]], "List All Hunks" },
-    H = { [[<cmd>Gitsigns setqflist<cr>]], "List Buffer Hunks" },
-    r = { [[<cmd>Gitsigns reset_hunk<cr>]], "Reset Hunk" },
-    R = { [[<cmd>Gitsigns reset_buffer<cr>]], "Reset Buffer" },
-    p = { [[<cmd>Gitsigns preview_hunk<cr>]], "Preview Hunk" },
-    b = { [[<cmd>lua require"gitsigns".blame_line{}<cr>]], "Blame" },
-    B = { [[<cmd>lua require"gitsigns".blame_line{full=true}<cr>]], "Blame Full Hunk" },
-    S = { [[<cmd>Gitsigns stage_buffer<cr>]], "Stage Buffer" },
-    U = { [[<cmd>Gitsigns reset_buffer_index<cr>]], "Reset Buffer Index" },
-}
--- q -> session
-local session_keymap = {
-    name = "state",
-    q = { "<cmd>qa<cr>", "Quit" },
-    Q = { "<cmd>wa<cr><cmd>qa<cr>", "Save and Quit" },
-    l = { require("my-config.session").list_sessions, "Search Sessions" },
-    s = { "<cmd>PossessionSave<cr>", "Save Session" },
-    t = { "<cmd>Telescope resume<cr>", "Resume Last Telescope Picker" },
-    T = { "<cmd>Telescope pickers<cr>", "Previous Telescope Pickers" },
-}
--- p -> project
-local project_keymap = {
-    name = "project",
-    t = { "<cmd>TodoTelescope<cr>", "Todo Items" },
-    f = { "<cmd>Telescope git_files<cr>", "Project Files" },
-}
--- n -> notes
-local notes_keymap = {
-    name = "notes",
-}
 
-wk.register({
-    b = buffer_keymap,
-    e = edit_keymap,
-    f = file_keymap,
-    i = code_keymap,
-    n = notes_keymap,
-    p = project_keymap,
-    q = session_keymap,
-    s = search_keymap,
-    t = toggle_feature_keymap,
-    v = vcs_keymap,
-    w = window_keymap,
-}, {
-    prefix = "<leader>",
+    { "<leader>i", group = "code actions" },
+    { "<leader>ia", [[<cmd>Telescope lsp_range_code_actions<cr>]], desc = "Code Actions" },
+    { "<leader>id", [[<cmd>Trouble workspace_diagnostics<cr>]], desc = "Workspace Diagnostics" },
+    { "<leader>iD", [[<cmd>Trouble document_diagnostics<cr>]], desc = "Document Diagnostics" },
+    { "<leader>ir", [[<cmd>Lspsaga rename<cr>]], desc = "Rename Symbol" },
+    { "<leader>is", [[<cmd>Telescope lsp_dynamic_workspace_symbols<cr>]], desc = "Workspace Symbols" },
+    { "<leader>iS", [[<cmd>Telescope lsp_document_symbols<cr>]], desc = "Document Symbols" },
+
+    { "<leader>n", group = "notes" },
+
+    { "<leader>p", group = "project" },
+    { "<leader>pf", [[<cmd>Telescope git_files<cr>]], desc = "Project Files" },
+    { "<leader>pt", [[<cmd>TodoTelescope<cr>]], desc = "Todo Items" },
+
+    { "<leader>q", group = "state" },
+    { "<leader>ql", require("my-config.session").list_sessions, desc = "Search Sessions" },
+    { "<leader>qq", [[<cmd>qa<cr>]], desc = "Quit" },
+    { "<leader>qQ", [[<cmd>wa<cr><cmd>qa<cr>]], desc = "Save and Quit" },
+    { "<leader>qs", [[<cmd>PossessionSave<cr>]], desc = "Save Session" },
+    { "<leader>qt", [[<cmd>Telescope resume<cr>]], desc = "Resume Last Telescope Picker" },
+    { "<leader>qT", [[<cmd>Telescope pickers<cr>]], desc = "Previous Telescope Pickers" },
+
+    { "<leader>s", group = "search" },
+    { "<leader>sf", [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>]], desc = "Search" },
+    {
+        "<leader>sF",
+        [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args({cwd="%:p:h", results_title=vim.fn.fnamemodify(vim.fn.expand("%:h"), ":~:.")})<cr>]],
+        desc = "Search in the directory of current file",
+    },
+    { "<leader>sh", [[<cmd>Telescope help_tags<cr>]], desc = "Help Tags" },
+    { "<leader>si", [[<cmd>Telescope treesitter<cr>]], desc = "Search Syntax Node" },
+    { "<leader>sk", [[<cmd>Telescope keymaps<cr>]], desc = "Keymap" },
+    { "<leader>sr", [[<cmd>lua require('spectre').open()<CR>]], desc = "Search and replace" },
+    { "<leader>sR", [[viw:lua require('spectre').open_file_search()<CR>]], desc = "Search and replace current symbol" },
+
+    { "<leader>t", group = "toggle features" },
+    { "<leader>tb", [[<cmd>Gitsigns toggle_current_line_blame<cr>]], desc = "Toggle Blame Line" },
+    { "<leader>tc", my_util.toggle_copilot_suppression, desc = "Toggle Copilot suppression" },
+    { "<leader>tC", [[<cmd>Copilot panel<cr>]], desc = "Toggle Copilot panel" },
+    { "<leader>td", [[<cmd>TroubleToggle<cr>]], desc = "Trouble Window" },
+    { "<leader>tf", require("zen-mode").toggle, desc = "Focus Mode" },
+    {
+        "<leader>tgb",
+        my_util.make_async_func(my_settings.prompt_setting, "git_base_branch"),
+        desc = "Set Git Base Branch",
+    },
+    {
+        "<leader>tgr",
+        my_util.make_async_func(my_settings.prompt_setting, "git_alternative_remote"),
+        desc = "Set Git Alternative Remote",
+    },
+    { "<leader>tn", [[<cmd>set invnumber<cr>]], desc = "Line Number" },
+    { "<leader>tp", [[<cmd>InspectTree<cr>]], desc = "Treesitter Playground" },
+    { "<leader>ts", require("auto-save").toggle, desc = "Auto Save" },
+    { "<leader>tS", [[<cmd>set invspell<cr>]], desc = "Spell Check" },
+
+    { "<leader>v", group = "version control" },
+    { "<leader>vb", [[<cmd>lua require"gitsigns".blame_line{}<cr>]], desc = "Blame" },
+    { "<leader>vB", [[<cmd>lua require"gitsigns".blame_line{full=true}<cr>]], desc = "Blame Full Hunk" },
+    { "<leader>vf", [[<cmd>Telescope git_status<cr>]], desc = "Git Status" },
+    { "<leader>vh", [[<cmd>Gitsigns setqflist all<cr>]], desc = "List All Hunks" },
+    { "<leader>vH", [[<cmd>Gitsigns setqflist<cr>]], desc = "List Buffer Hunks" },
+    { "<leader>vl", my_git.copy_link_to_remote, desc = "Copy link to repo remote" },
+    { "<leader>vL", my_git.copy_link_to_alternative_remote, desc = "Copy link to alternative remote" },
+    { "<leader>vp", [[<cmd>Gitsigns preview_hunk<cr>]], desc = "Preview Hunk" },
+    { "<leader>vr", [[<cmd>Gitsigns reset_hunk<cr>]], desc = "Reset Hunk" },
+    { "<leader>vR", [[<cmd>Gitsigns reset_buffer<cr>]], desc = "Reset Buffer" },
+    { "<leader>vs", [[<cmd>Gitsigns stage_hunk<cr>]], desc = "Stage Hunk" },
+    { "<leader>vS", [[<cmd>Gitsigns stage_buffer<cr>]], desc = "Stage Buffer" },
+    { "<leader>vu", [[<cmd>Gitsigns undo_stage_hunk<cr>]], desc = "Undo Stage Hunk" },
+    { "<leader>vU", [[<cmd>Gitsigns reset_buffer_index<cr>]], desc = "Reset Buffer Index" },
+    { "<leader>vv", [[<cmd>lua require("my-config.terminal").toggle_lazygit()<cr>]], desc = "Open lazygit" },
+
+    { "<leader>w", group = "window" },
+    { "<leader>wo", my_window.move_to_next_window, desc = "Move Buffer to Next Window" },
+    { "<leader>wO", my_window.open_in_next_window, desc = "Open Buffer in Next Window" },
+    {
+        "<leader>wp",
+        [[<cmd>lua require'my-config.window'.move_to_next_window(true)<cr>]],
+        desc = "Move Buffer to Next Window And Enter",
+    },
+    { "<leader>ww", leap_to_window, desc = "Jump to Window" },
+
+    { "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", desc = "Next Hunk", expr = true },
+    { "[c", "&diff ? '[c' : '<cmd>Gitsigns prev<CR>'", desc = "Previous Hunk", expr = true },
+
+    { "gl", "<cmd>HopLineStart<cr>", desc = "Hop Line" },
 })
 
-wk.register({
-    ["]c"] = { "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", "Next Hunk", expr = true },
-    ["[c"] = { "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", "Previous Hunk", expr = true },
-}, { mode = "n", prefix = "" })
-
--- Hop
-wk.register({
-    l = { "<cmd>HopLineStart<cr>", "Hop Line" },
-    h = {
-        name = "other hops",
-        w = { [[<cmd>lua require"hop-extensions".hint_cword()<cr>]], "Hop Current Word" },
-        W = { [[<cmd>lua require"hop-extensions".hint_cWORD()<cr>]], "Hop Current WORD" },
-        l = { [[<cmd>lua require"hop-extensions".hint_locals()<cr>]], "Hop Locals" },
-        s = { [[<cmd>lua require"hop-extensions".hint_scopes()<cr>]], "Hop Scopes" },
-        d = { [[<cmd>lua require"hop-extensions".hint_definitions()<cr>]], "Hop Definitions" },
-        k = { [[<cmd>lua require"hop-extensions".hint_textobjects('function')<cr>]], "Hop Functions" },
-        a = { [[<cmd>lua require"hop-extensions".hint_textobjects('parameter')<cr>]], "Hop Parameter" },
-    },
-}, { mode = "n", prefix = "g" })
-
--- Text object labels
-local text_objects = {
-    -- tree-sitter
-    ["af"] = "function",
-    ["if"] = "inner function",
-    ["ac"] = "class",
-    ["ic"] = "inner class",
-    ["aa"] = "parameter",
-    ["ia"] = "inner parameter",
-    -- textobj-entire
-    ["ae"] = "entire buffer",
-    ["ie"] = "entire buffer (without surrounding empty lines)",
-    -- gitsign
-    ["ih"] = { ":<C-U>Gitsigns select_hunk<CR>", "Git Hunk" },
-}
-wk.register(text_objects, { mode = "o", prefix = "" })
-wk.register(text_objects, { mode = "x", prefix = "" })
+wk.add({
+    { "ih", ":<C-U>Gitsigns select_hunk<CR>", desc = "Git Hunk", mode = { "o", "x" } },
+})
 
 local function mapkey(lhs, modes, rhs, opts, desc)
     -- shortcut for vim.keymap.set so that lhs will be
@@ -294,15 +246,6 @@ local function mapkey(lhs, modes, rhs, opts, desc)
         opts = vim.tbl_extend("force", opts, { desc = desc })
     end
     vim.keymap.set(modes, lhs, rhs, opts)
-end
-
-local function goto_start_of_line()
-    local current_col = vim.api.nvim_win_get_cursor(0)[2]
-    vim.api.nvim_command([[normal! ^]])
-    local new_col = vim.api.nvim_win_get_cursor(0)[2]
-    if new_col == current_col then
-        vim.api.nvim_command([[normal! 0]])
-    end
 end
 
 mapkey("0", { "n", "x" }, goto_start_of_line)
