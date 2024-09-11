81f2569b4dc0436e8067cd5f9b47475ab44e9d5a
Lite Ye <yelite958@gmail.com>
2024-09-10 20:26:02 -0400

    neovim: fix gitlinker

--- home/neovim/lua/my-config/git.lua
+++ home/neovim/lua/my-config/git.lua
@@ -6,11 +6,11 @@ local my_settings = require("my-config.settings")
 local M = {}
 
 function M.copy_link_to_remote()
-    gitlinker.get_buf_range_url("n", {})
+    gitlinker.link({})
 end
 
 M.copy_link_to_alternative_remote = my_util.make_async_func(function()
-    gitlinker.get_buf_range_url("n", { remote = my_settings.get_or_prompt_setting("git_alternative_remote", "origin") })
+    gitlinker.link({ remote = my_settings.get_or_prompt_setting("git_alternative_remote", "origin") })
 end)
 
 return M
