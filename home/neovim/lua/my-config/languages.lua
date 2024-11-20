30cdbcc1215aaa3a4bf59277199f36f18223c045
Lite Ye <yelite958@gmail.com>
2024-11-20 00:12:38 -0500

    neovim: use gpt-4o for avante

--- home/neovim/lua/my-config/languages.lua
+++ home/neovim/lua/my-config/languages.lua
@@ -112,7 +112,7 @@ if util.is_copilot_installed() then
     require("avante").setup({
         provider = "copilot",
         copilot = {
-            model = "claude-3.5-sonnet",
+            model = "gpt-4o-2024-08-06",
             timeout = 30000,
             temperature = 0,
             max_tokens = 4096,
