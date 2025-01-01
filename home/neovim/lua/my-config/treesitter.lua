ac34a0dea0b5cac24d4132312b94bd3a73f4e92c
Lite Ye <yelite958@gmail.com>
2025-01-01 13:54:13 -0500

    neovim: disable json conceal

--- home/neovim/lua/my-config/treesitter.lua
+++ home/neovim/lua/my-config/treesitter.lua
@@ -80,3 +80,41 @@ require("nvim-treesitter.configs").setup({
         },
     },
 })
+
+vim.treesitter.query.set(
+    "json",
+    "highlights",
+    [[
+[
+  (true)
+  (false)
+] @boolean
+
+(null) @constant.builtin
+
+(number) @number
+
+(pair
+  key: (string) @property)
+
+(pair
+  value: (string) @string)
+
+(array
+  (string) @string)
+
+[
+  ","
+  ":"
+] @punctuation.delimiter
+
+[
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+(escape_sequence) @string.escape
+    ]]
+)
