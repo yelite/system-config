c95a14bb3f0cf354069b31e09f170c60ec41fac8
Lite Ye <yelite958@gmail.com>
2025-12-13 11:13:09 -0500

    use the latest claude code

--- overlays/extra-pkgs/default.nix
+++ overlays/extra-pkgs/default.nix
@@ -10,6 +10,15 @@ final: prev:
   flameshot = prev.flameshot.overrideAttrs (old: {
     patches = old.patches ++ [./patches/flameshot.patch];
   });
+
+  claude-code = prev.claude-code.overrideAttrs (finalAttrs: previousAttrs: {
+    version = "2.0.65";
+
+    src = final.fetchzip {
+      url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
+      hash = "sha256-LcHY7pdnRdeEwtb1Fi/GW6D5Rv3StiKsoXQc3CPDmQw=";
+    };
+  });
 }
 // prev.lib.optionalAttrs prev.stdenv.isDarwin {
   hammerspoon = final.callPackage ./hammerspoon.nix {};
