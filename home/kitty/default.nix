ae63074b6d3981821b9948703f26ed1ecea4e96a
Lite Ye <yelite958@gmail.com>
2026-02-25 14:42:43 -0500

    Revert "kitty restore ctrl-tab key"
    This reverts commit 6376fdcce041f982b83b99607bd533792fc181f5.

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -35,6 +35,8 @@ lib.mkIf config.myConfig.kitty.enable {
     shellIntegration.mode = "no-sudo";
     themeFile = "Tomorrow_Night_Eighties";
     keybindings = {
+      "ctrl+tab" = "no_op";
+      "ctrl+shift+tab" = "no_op";
       "super+c" = "copy_to_clipboard";
       "super+v" = "paste_from_clipboard";
       "super+t" = "new_tab";
