63bd843ab8f2565d94d46610cc9c0fc37291a2b2
Lite Ye <yelite958@gmail.com>
2026-02-28 00:28:22 -0500

    macos: keybind to focus next window

--- home/hammerspoon/init.lua
+++ home/hammerspoon/init.lua
@@ -3,3 +3,16 @@ require("hs.ipc")
 local hyper = { "ctrl", "shift", "option" }
 
 hs.hotkey.bind(hyper, "1", hs.caffeinate.systemSleep)
+
+hs.hotkey.bind({ "option" }, "p", function()
+    local win = hs.window.focusedWindow()
+    if not win then
+        return
+    end
+    local nextScreen = win:screen():next()
+    local wf = hs.window.filter.new():setCurrentSpace(true):setScreens(nextScreen:id())
+    local windows = wf:getWindows(hs.window.filter.sortByFocusedLast)
+    if #windows > 0 then
+        windows[1]:focus()
+    end
+end)
